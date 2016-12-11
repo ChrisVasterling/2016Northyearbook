d2fc94781857f32fb0994c046af64482a380920a
Chris Vasterling <chrisvasterling@gmail.com>
2016-12-11 17:17:20 -0600

    header

--- js/main.js
+++ js/main.js
@@ -11,11 +11,31 @@ function bottomSlide(btn) {
 		btnId.style.opacity = "0.0";
 	}, 400)
 }
-function closeMenu() {
+function closeMenu(cover_delay) {
     var menu = document.getElementById('navMain');
     setTimeout( function() {
         menu.style.transform = 'translate3d(-250px, 0px, 0px)';
-    }, 400)
+        closeMenuCover();
+    }, cover_delay)
+}
+function openMenu() {
+    var menu = document.getElementById('navMain');
+    menu.style.transform = 'translate3d(0px, 0px, 0px)';
+    openMenuCover();
+}
+function closeMenuCover() {
+    cvr = document.getElementById('cover');
+    cvr.style.backgroundColor = 'transparent';
+    setTimeout( function () {
+        cvr.style.display = 'none';
+    }, 300)
+}
+function openMenuCover() {
+    cvr = document.getElementById('cover');
+    cvr.style.display = 'block';
+    setTimeout( function () {
+        cvr.style.backgroundColor = 'rgba(0, 0, 0, .75)';
+    }, 0)
 }
 function JSLink(btn) {
     url = document.getElementById(btn).dataset.url
